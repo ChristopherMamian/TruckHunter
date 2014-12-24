398cfa6bb13ceb959ce0011752c331f8f22ad361
Nick Perez <nickperez1285@gmail.com>
2014-12-23 17:23:37 -0800

    made mongoose suggestions

--- server/schemas/truck.js
+++ server/schemas/truck.js
@@ -1,8 +1,27 @@
 var mongoose = require('mongoose');
 
+
 module.exports = mongoose.model('Truck', {
 	truckName: String,
 	currentAddress: String,
 	foodType: String,
 	active: Boolean,
 });
+
+
+
+// added by nick => not sure what the module.exports is for 
+// code below should work if above does not 
+
+
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema 
+
+// var trucksSchema = new Schema ({
+// 	truckName: String,
+// 	currentAddress: String,
+// 	foodType: String,
+// 	active: Boolean,
+// });
+
+// mongoose.model('trucks', trucksSchema);

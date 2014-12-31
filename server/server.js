78c27d3f9b2c3f304f7407eed4bf8dab2295fc14
Samantha Stallings <samantha.stallings1@gmail.com>
2014-12-31 10:45:01 +0100

    still broken

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     next();
--- twitter_parser/twitter_handles.rb
+++ twitter_parser/twitter_handles.rb
@@ -4,29 +4,29 @@ require 'open-uri'
 
 #### Run this script first and grab twitter links ####
 
-twitter_links_list ='google_links.txt'
-twitter_links_array = []
+# twitter_links_list ='google_links.txt'
+# twitter_links_array = []
 
-File.readlines(twitter_links_list).each do |line|
-  twitter_links_array << line
-end
+# File.readlines(twitter_links_list).each do |line|
+#   twitter_links_array << line
+# end
 
-twitter_links_array.map! do |link|
-  #slices off "http://www.google.com/search?q="
-  #then removes all special characters and numbers
-  link.slice!(31..-9).gsub(/[^a-zA-Z\-]/,"")
-end
+# twitter_links_array.map! do |link|
+#   #slices off "http://www.google.com/search?q="
+#   #then removes all special characters and numbers
+#   link.slice!(31..-9).gsub(/[^a-zA-Z\-]/,"")
+# end
 
-#next we need to add the twitter url to the beginning
-# https://twitter.com/
+# #next we need to add the twitter url to the beginning
+# # https://twitter.com/
 
-twitter_links_array.map! do |link|
-  link.prepend("https://twitter.com/")
-end
+# twitter_links_array.map! do |link|
+#   link.prepend("https://twitter.com/")
+# end
 
-twitter_links_array.each do |link|
-  puts link
-end
+# twitter_links_array.each do |link|
+#   puts link
+# end
 
 #### Run this script second to grab handles and imgs ####
 #### Comment out everything above before running script ####
@@ -40,6 +40,10 @@ twitter_urls_list = []
 File.readlines(handles).each do |line|
   twitter_urls_list << line
 end
+
+# twitter_urls_list.each do |url|
+#   # p url
+# end
 
 
 stripped_handles = []
